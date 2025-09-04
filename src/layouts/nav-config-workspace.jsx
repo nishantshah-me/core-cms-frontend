d3b4a020f9917877f778522edc04004220c2b087
AAkhade <akshay.akhade@hexafoldtech.com>
2025-09-04 10:31:12 +0530

    base structure for core cms

---
+++ src/layouts/nav-config-workspace.jsx
@@ -0,0 +1,43 @@
+import { CONFIG } from 'src/global-config';
+
+// ----------------------------------------------------------------------
+
+export const _workspaces = [
+  {
+    id: 'team-1',
+    name: 'Officeous',
+    // plan: 'Free',
+    logo: `${CONFIG.assetsDir}/assets/icons/workspaces/logo-1.webp`,
+  },
+  {
+    id: 'team-2',
+    name: 'Hexafold',
+    // plan: 'Pro',
+    logo: `${CONFIG.assetsDir}/assets/icons/workspaces/logo-2.webp`,
+  },
+  {
+    id: 'team-3',
+    name: 'Zeevaara',
+    // plan: 'Pro',
+    logo: `${CONFIG.assetsDir}/assets/icons/workspaces/logo-3.webp`,
+  },
+
+  {
+    id: 'team-4',
+    name: 'Nivipi',
+    // plan: 'Free',
+    logo: `${CONFIG.assetsDir}/assets/icons/workspaces/logo-1.webp`,
+  },
+  {
+    id: 'team-5',
+    name: 'InvoiceMaker',
+    // plan: 'Pro',
+    logo: `${CONFIG.assetsDir}/assets/icons/workspaces/logo-2.webp`,
+  },
+  {
+    id: 'team-6',
+    name: 'Blueray',
+    // plan: 'Pro',
+    logo: `${CONFIG.assetsDir}/assets/icons/workspaces/logo-3.webp`,
+  },
+];
