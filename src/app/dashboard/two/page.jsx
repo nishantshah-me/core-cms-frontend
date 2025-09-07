f7d9b8c670b19c7322a403da1d90ce38e5a984ad
AAkhade <akshay.akhade@hexafoldtech.com>
2025-09-07 19:17:59 +0530

    testing git command

--- src/app/dashboard/two/page.jsx
+++ src/app/dashboard/two/page.jsx
@@ -7,5 +7,5 @@ import { BlankView } from 'src/sections/blank/view';
 export const metadata = { title: `Page two | Dashboard - ${CONFIG.appName}` };
 
 export default function Page() {
-  return <BlankView title="Page two" />;
+  return <BlankView title="Page two " />;
 }
